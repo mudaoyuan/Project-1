b03cdc7c21141c8413342880d110c1e1a12084d0
lijing <13437383+mudaoyuan@user.noreply.gitee.com>
2023-12-12 16:44:07 +0800

    fix: 存储

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,13 +1,13 @@
 // 封装本地存储数据与读取数据反复噶
 export const SET_TOKEN = (token: string) => {
-  localStorage.setItem('TOKEN', token)
+  sessionStorage.setItem('TOKEN', token)
 }
 
 export const GET_TOKEN = () => {
-  return localStorage.getItem('TOKEN')
+  return sessionStorage.getItem('TOKEN')
 }
 
 //本地存储删除数据
 export const REMOVE_TOKEN = () => {
-  localStorage.removeItem('TOKEN')
+  sessionStorage.removeItem('TOKEN')
 }
